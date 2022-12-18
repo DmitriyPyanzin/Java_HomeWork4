06d55240620be8bd4763198847b702494b76fee9
Dmitry_Pyanzin <lysy_41@mail.ru>
2022-12-18 15:08:28 +0300

    finish_HomeWork4

--- src/Task2.java
+++ src/Task2.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
 
     /*
@@ -8,6 +12,30 @@ public class Task2 {
      */
 
     public static void main(String[] args) {
-        System.out.println();
+        Task2 t2 = new Task2();
+        LinkedList<String> linkedList = new LinkedList<>();
+        t2.fillList(linkedList);
+        System.out.println(linkedList);
+        System.out.println(t2.reverse(linkedList));
+    }
+
+    private <T> List<T> reverse(LinkedList<T> source) {
+        LinkedList<T> newList = new LinkedList<>();
+        int len = source.size();
+        for (int i = 0; i < len; i++) {
+            newList.add(source.getLast());
+            source.removeLast();
+        }
+        return newList;
+    }
+
+    private <T> void fillList(LinkedList<T> list) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Какой длины будет список?");
+        int len = sc.nextInt();
+        System.out.println("Заполняем список");
+        for (int i = 0; i < len; i++) {
+            list.add((T) sc.next());
+        }
     }
 }
